49ea7ac61b6a96e1381e2a8fcffd72a80b4ca242
rauldimon <rauldimon2@gmail.com>
2020-02-11 14:50:39 +0200

    Updates

---
+++ precache-manifest.92615d9398b63ad1142120049c3545be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7200bb22eac3c8d7da2b4e8a577c3c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5504704d0532dd86abce",
+    "url": "/static/css/main.9269e750.chunk.css"
+  },
+  {
+    "revision": "4a7d251644c2f7657e6b",
+    "url": "/static/js/2.fc5b8106.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fc5b8106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5504704d0532dd86abce",
+    "url": "/static/js/main.01dce43a.chunk.js"
+  },
+  {
+    "revision": "fc6022a8ef0a8f8ba8cb",
+    "url": "/static/js/runtime-main.02a6d730.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/static/media/252.e45d9d1f.png"
+  }
+]);
